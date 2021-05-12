a15faa38016a220b1e7bfc798cb074dad2e92f71
jleopold88 <jleopold88@yahoo.com>
2021-05-12 21:40:49 +0700

    Login & Register fungsionalitas

--- app/src/main/java/umn/ac/id/uas_mobile/Login_activity.java
+++ app/src/main/java/umn/ac/id/uas_mobile/Login_activity.java
@@ -1,43 +1,86 @@
 package umn.ac.id.uas_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login_activity extends AppCompatActivity {
-    EditText Username, Password;
-    Button btLogin;
+    EditText Username, Password, Email_user;
+    Button btLogin,btRegister;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Username = findViewById(R.id.username);
+        Email_user = findViewById(R.id.username);
         Password = findViewById(R.id.password);
         btLogin = findViewById(R.id.login);
+        btRegister = findViewById(R.id.reg);
+        fAuth = FirebaseAuth.getInstance();
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-                if(Username.getText().toString().equals("uasmobile") && Password.getText().toString().equals("uasmobilegenap")){
-                    openListActivity();
+                String email = Email_user.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                if(TextUtils.isEmpty(email))
+                {
+                    Email_user.setError("Email is Required !");
+                    return;
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"Invalid Username / Password",Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(password) || password.length() < 8)
+                {
+                    Password.setError("Password must be at least 8 Characters !");
+                    return;
                 }
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(Login_activity.this, "Logged In", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(Login_activity.this,"Error !"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterActivity();
             }
         });
     }
-    public void openListActivity()
+    public void openMainActivity()
     {
-        Intent intent = new Intent(this, Home_activity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void openRegisterActivity()
+    {
+        startActivity(new Intent(this, Register_activity.class));
     }
 }
--- app/src/main/java/umn/ac/id/uas_mobile/Register_activity.java
+++ app/src/main/java/umn/ac/id/uas_mobile/Register_activity.java
@@ -2,16 +2,24 @@ package umn.ac.id.uas_mobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register_activity extends AppCompatActivity {
     EditText Username, Password, Nama, Email_user, Domisili, Umur;
     Button btnSubmit;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,13 @@ public class Register_activity extends AppCompatActivity {
         Email_user = findViewById(R.id.email_user);
         Domisili = findViewById(R.id.domisili);
         Umur = findViewById(R.id.umur);
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
 
 //        Register Button On Click broh
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -32,12 +47,52 @@ public class Register_activity extends AppCompatActivity {
             public void onClick(View v)
             {
 //                Lempar ke DB bro
-                if(Username.getText().toString().equals("uasmobile") && Password.getText().toString().equals("uasmobilegenap")){
-                    openListActivity();
+                String email = Email_user.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String username = Username.getText().toString().trim();
+                String domisili = Domisili.getText().toString().trim();
+                int umur = Integer.parseInt(String.valueOf(Umur.getText()));
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Email_user.setError("Email is Required !");
+                    return;
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"Invalid Username / Password",Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(password) || password.length() < 8)
+                {
+                    Password.setError("Password must be at least 8 Characters !");
+                    return;
                 }
+                if(TextUtils.isEmpty(username))
+                {
+                    Username.setError("Username is Required !");
+                    return;
+                }
+                if (TextUtils.isEmpty(domisili))
+                {
+                    Domisili.setError("Domisili is Required !");
+                }
+                if (umur == 0)
+                {
+                    Umur.setError("Umur is Reqquired !");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(Register_activity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),Login_activity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(Register_activity.this,"Error !"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }// End public on click view
         });//btn submit on click
     }// on create
