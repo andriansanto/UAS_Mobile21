3763434202a9d54b42e98f2dbd5fd77f62bc251e
jleopold88 <jleopold88@yahoo.com>
2021-05-15 20:17:27 +0700

    konek firebase ke punya Ask form pickup

--- app/src/main/java/umn/ac/id/uas_mobile/PickupFormActivity.java
+++ app/src/main/java/umn/ac/id/uas_mobile/PickupFormActivity.java
@@ -1,24 +1,44 @@
 package umn.ac.id.uas_mobile;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.SetOptions;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class PickupFormActivity extends AppCompatActivity {
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
     private TextView tvDateResult;
-    private Button btDatePicker;
+    private Button btDatePicker, btnSubmit;
+    private TextView Username;
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +46,21 @@ public class PickupFormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        Username = findViewById(R.id.recash3);
+        btnSubmit = findViewById(R.id.btn_submitPickup);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        //Firesbase fetch data and display//
+        DocumentReference documentReference = fStore.collection("Users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                Username.setText(value.getString("Username"));
+            }
+        });
 
         tvDateResult = (TextView) findViewById(R.id.tv_dateresult);
         btDatePicker = (Button) findViewById(R.id.bt_datepicker);
@@ -33,6 +68,18 @@ public class PickupFormActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showDateDialog();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // Add a new document with a generated ID
+                Map<String, Object> data = new HashMap<>();
+                data.put("capital", true);
+
+                fStore.collection("Users").document(userID)
+                        .set(data, SetOptions.merge());
             }
         });
 
